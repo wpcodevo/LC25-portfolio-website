96a5b01333eab8986ecdb1d541e21fd734c3b4b0
Edem <ziddah.edem@gmail.com>
2021-11-04 10:50:12 +0000

    updated

--- js/main.js
+++ js/main.js
@@ -65,6 +65,27 @@ const swiper = new Swiper(".mySwiper", {
   },
 });
 
+/* =========== Scroll Top =========== */
+const scrollTop = document.querySelector(".scroll-top");
+
+scrollTop.addEventListener("click", () => {
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+window.addEventListener("scroll", (e) => {
+  const scrollHeight = window.pageYOffset;
+
+  if (scrollHeight > 300) {
+    scrollTop.classList.add("show");
+  } else {
+    scrollTop.classList.remove("show");
+  }
+});
+
 /* ========== Scroll Reveal ============ */
 const scroll = ScrollReveal({
   distance: "60px",
